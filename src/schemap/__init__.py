03e094cef600f1c1b4aba6b60a55fffd54ba085d
Emiliano Gandini Outeda <emiliano.gandini@protonmail.com>
2026-05-31 00:17:43 -0300

    feat: SchemaConfig

--- src/schemap/__init__.py
+++ src/schemap/__init__.py
@@ -1,11 +1,12 @@
 """ Automatic Pydantic schemas for SQLAlchemy models """
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 from schemap.base import AutoBase, SchemaMixin
 from schemap.builder import build_schema
 from schemap.types import extract_python_type, extract_column_metadata
 from schemap.utils.likeness import ColumnLike
+from schemap.config import SchemaConfig
 
 __all__ = [
     "AutoBase",
@@ -14,5 +15,5 @@ __all__ = [
     "extract_python_type",
     "extract_column_metadata",
     "ColumnLike",
+    "SchemaConfig",
 ]
-
